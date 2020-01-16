2101a5beceaddf15585e9fab4a710737c317a098
matthewjflee <matthewjflee@hotmail.com>
2020-01-16 08:34:31 -0700

    First commit

---
+++ src/main/java/ca/sait/vezorla/VezorlaApplication.java
@@ -0,0 +1,13 @@
+package ca.sait.vezorla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VezorlaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VezorlaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ca/sait/vezorla/VezorlaApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.sait.vezorla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VezorlaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
