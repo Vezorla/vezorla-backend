8279a5deb0cea27a8bcb9ee0a68db131092eae9c
Kwistech <johnkwisses@gmail.com>
2020-04-06 20:46:10 -0600

    Complete Javadoc for package: vezorla/VezorlaApplication.java

--- src/main/java/ca/sait/vezorla/VezorlaApplication.java
+++ src/main/java/ca/sait/vezorla/VezorlaApplication.java
@@ -2,11 +2,24 @@ package ca.sait.vezorla;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
+/**
+ * VezorlaApplication class.
+ *
+ * This class is the main class for the backend program.
+ *
+ * The SpringBootApplication annotation denotes this program
+ * as a Spring Boot Application and uses Spring annotation
+ * through the programs classes, methods, and variables.
+ */
 @SpringBootApplication
 public class VezorlaApplication {
 
+    /**
+     * Main method of the VezorlaApplication.
+     *
+     * @param args Optional Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         SpringApplication.run(VezorlaApplication.class, args);
     }
